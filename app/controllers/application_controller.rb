b47ca3a157d4f4b1efd80a464d1dfc61a06a3c6e
Tark Sininelind <tark@maxxima.net>
2017-06-13 18:34:54 +0000

    Fix missing : in app_con.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-  render html "Tierra y libertad!" 
+  render html: "Tierra y libertad!" 
   end
 end
